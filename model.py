99afd9fee4558db42604d21c225bfbe405c5ac36
Alex Poplawski <alex0poplawski@gmail.com>
2019-06-07 16:08:14 -0700

    version 2

--- model.py
+++ model.py
@@ -1,61 +1,86 @@
-from keras.models import Sequential
+from keras.models import Model
 from keras.datasets import mnist
 from keras.layers import Dense, UpSampling2D
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Reshape
 import sys
 from keras import backend as K
-
+import matplotlib as plt
 ACTIVATION = 'relu'
-
+(x_train, _), (x_test, _) = mnist.load_data()
 
 
 batch_size = 128
 epochs = 12
 
 img_rows, img_cols = 28, 28
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
 
+x = Conv2D(8, (3, 3), activation=ACTIVATION, padding='same')(input_img)
+x = Conv2D(8, (3, 3), activation=ACTIVATION, padding='same')(x)
 
-model = Sequential()
-model.add(Conv2D(8, (3, 3), activation=ACTIVATION,
-                 padding='same', input_shape=input_shape))
-model.add(Conv2D(8, (3, 3), activation=ACTIVATION,
-                 padding='same'))
-model.add(MaxPooling2D((2, 2), padding='same'))
-model.add(Conv2D(16, (3, 3), activation=ACTIVATION,
-                 padding='same'))
-model.add(Conv2D(16, (3, 3), activation=ACTIVATION,
-                 padding='same'))
-model.add(MaxPooling2D((2, 2), padding='same'))
-model.add(Flatten())
-model.add(Dense(256))
-model.add(Dense(784))
-model.add(Reshape((16, 7, 7)))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(16, (3, 3), activation=ACTIVATION,
-                 padding='same'))
-model.add(Conv2D(16, (3, 3), activation=ACTIVATION,
-                 padding='same'))
-model.add(UpSampling2D((2, 2)))
-model.add(Conv2D(8, (3, 3), activation=ACTIVATION,
-                 padding='same'))
-model.add(Conv2D(1, (3, 3), activation=ACTIVATION, padding='same'))
-model.compile(optimizer='rmsprop',
-              metrics='mse')
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+x = MaxPooling2D((2, 2), padding='same')(x)
+
+x = Conv2D(16, (3, 3), activation=ACTIVATION, padding='same')(x)
+x = Conv2D(16, (3, 3), activation=ACTIVATION, padding='same')(x)
+
+x = MaxPooling2D((2, 2), padding='same')(x)
+
+x = Conv2D(8, (3, 3), activation=ACTIVATION, padding='same')(x)
+x = Conv2D(8, (3, 3), activation=ACTIVATION, padding='same')(x)
+
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+x = Conv2D(8, (3, 3), activation=ACTIVATION, padding='same')(encoded)
+x = Conv2D(8, (3, 3), activation=ACTIVATION, padding='same')(x)
+
+x = UpSampling2D((2, 2))(x)
+
+x = Conv2D(16, (3, 3), activation=ACTIVATION, padding='same')(x)
+x = Conv2D(16, (3, 3), activation=ACTIVATION, padding='same')(x)
+
+x = UpSampling2D((2, 2))(x)
+
+x = Conv2D(16, (3, 3), activation=ACTIVATION, padding='same')(x)
+x = Conv2D(16, (3, 3), activation=ACTIVATION, padding='same')(x)
+
+x = UpSampling2D((2, 2))(x)
+
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimzer='adadelta', loss='binary_crossentropy')
+
+import numpy as np
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+decoded_imgs = autoencoder.predict(x_test)
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
